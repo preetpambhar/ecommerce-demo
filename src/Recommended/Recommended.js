0f5e142c9842d23cec0399d5909d6cb16d70affb
Preet-Patel <70093554+Preet-Patel@users.noreply.github.com>
2024-02-03 02:41:46 -0500

    feat: recommended buttons

--- src/Recommended/Recommended.js
+++ src/Recommended/Recommended.js
@@ -1,6 +1,17 @@
 import "./Recommended.css";
 function Recommended() {
-  return <div>Recommended</div>;
+  return <>
+  <div>
+    <h2 className="recommended-title">Recommended</h2>
+    <div className="recommended-flex">
+      <button className="btns">All Products</button>
+      <button className="btns">Nike</button>
+      <button className="btns">Adidas</button>
+      <button className="btns">Puma</button>
+      <button className="btns">Bata</button>
+    </div>
+    </div>
+    </>
 }
 
 export default Recommended;
