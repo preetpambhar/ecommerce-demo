317f2d93fb1fab201a66b68370856e3524fe0184
Preet-Patel <70093554+Preet-Patel@users.noreply.github.com>
2024-02-05 00:34:05 -0500

    chore: move search bar into product page

--- src/Recommended/Recommended.js
+++ src/Recommended/Recommended.js
@@ -1,6 +1,8 @@
 import Buttons from "../components/Buttons";
 import "./Recommended.css";
-function Recommended({handleClick}) {
+import '../Navigation/Nav.css'
+
+function Recommended({handleClick, handleInputChange, query}) {
   return <>
   <div>
     <h2 className="recommended-title">Recommended</h2>
@@ -10,6 +12,15 @@ function Recommended({handleClick}) {
       <Buttons onClickHandler={handleClick} value="Adidas" title="Adidas"/>
       <Buttons onClickHandler={handleClick} value="Puma" title="Puma"/>
       <Buttons onClickHandler={handleClick} value="Vans" title="Vans"/>
+      <div className="nav-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Enter your shoes search."
+          onChange={handleInputChange}
+          value={query}
+        />
+      </div>
     </div>
     </div>
     </>
--- src/Slidbar/Sidebar.js
+++ src/Slidbar/Sidebar.js
@@ -4,9 +4,6 @@ import "./Sidebar.css";
 function Sidebar({ handleChange }) {
   return (
     <section className="sidebar">
-      <div className="logo-container">
-        <h1>🛒</h1>
-      </div>
       <Category handleChange={handleChange}/>
     </section>
   );
