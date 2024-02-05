b66b058a2f87e861b041dd5ab0d6e9c5f55106ec
Preet-Patel <70093554+Preet-Patel@users.noreply.github.com>
2024-02-05 00:35:28 -0500

    chore: setup router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Cart from "./Cart/Cart";
+import Account from "./Account/Account";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ProductView from "./Products/ProductView";
+import Layout from "./Layout/Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "", element: <App /> },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      {
+        path: "account",
+        element: <Account />,
+      },
+      {
+        path: "productview",
+        element: <ProductView />,
+      }
+    ],
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
